440bd49f802e18d718aa08635910224dd3ffcf11
yorifuji <yorifuji@gmail.com>
2024-08-07 08:39:03 +0900

    ci: Update workflow to use changelog URL

--- get_latest_flutter_version.sh
+++ get_latest_flutter_version.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-rm -rf tmp
-mkdir tmp
-cd tmp
-#git clone --depth 1 --branch main git@github.com:flutter/flutter.git
-gh repo clone flutter/flutter
+curl -O https://raw.githubusercontent.com/flutter/flutter/main/CHANGELOG.md
 
-filePath="./flutter/docs/releases/Hotfixes-to-the-Stable-Channel.md"
-#VERSION=$(grep -o -m 1 '### \[[0-9]\+\.[0-9]\+\.[0-9]\+\]' "$filePath" | sed -E 's/[^0-9.]//g')
-VERSION=$(grep -o -m 1 '### \[[0-9]\+\.[0-9]\+\.[0-9]\+\]' "$filePath" | sed 's/[^0-9.]//g')
+VERSION=$(grep -o -m 1 '### \[[0-9]\+\.[0-9]\+\.[0-9]\+\]' CHANGELOG.md | sed 's/[^0-9.]//g')
 echo "Version: $VERSION"
 
 echo $VERSION > version.txt
